e500736eed8886b233f17321b07affc57701b18d
Aifedayo <akeemifedayolag@gmail.com>
2021-04-05 12:00:00 +0100

    polls/models.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,6 +4,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
