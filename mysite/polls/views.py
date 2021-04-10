58bba8371974b1ec63656bf91e77085100323491
Aifedayo <akeemifedayolag@gmail.com>
2021-04-10 12:00:00 +0100

    polls/views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index")
 
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
 
-
-def result(request, question_id):
+def results(request, question_id):
     response = "You are looking at the results of question %s."
     return HttpResponse(response % question_id)
 
