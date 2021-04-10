680e363f99ea71af3f1ee622bd2807babdba99aa
Aifedayo <akeemifedayolag@gmail.com>
2021-04-10 12:00:00 +0100

    polls/views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,3 +4,9 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index")
 
 
+
+def result(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
