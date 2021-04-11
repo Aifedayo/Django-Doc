13f73caf7552c35b883438f1a347fe31403e8f0f
Aifedayo <akeemifedayolag@gmail.com>
2021-04-11 12:00:00 +0100

    polls/views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,4 +10,5 @@ def results(request, question_id):
     response = "You are looking at the results of question %s."
     return HttpResponse(response % question_id)
 
-
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
