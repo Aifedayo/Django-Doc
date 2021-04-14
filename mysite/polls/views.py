2a8ade76949e1a79288a9e0d7ae57601473db364
Aifedayo <akeemifedayolag@gmail.com>
2021-04-14 12:00:00 +0100

    polls/views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,7 +4,7 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse("Hello, world. You're at the polls index")
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s." % question_id)
