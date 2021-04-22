6d5f928a2413e64b405975276fec36d84975dc6d
Aifedayo <akeemifedayolag@gmail.com>
2021-04-22 12:00:00 +0100

    polls/views.py Http404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -15,6 +15,10 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exists")
     return HttpResponse("You are looking at question %s." % question_id)
 
 def results(request, question_id):
