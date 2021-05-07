cddc948beaacb5cd4350fd5343432d10356a80ec
Aifedayo <akeemifedayolag@gmail.com>
2021-05-07 12:00:00 +0100

    polls/views.py form handling

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.template import loader
+#from django.template import loader
 from .models import Choice, Question
 
 def index(request):
