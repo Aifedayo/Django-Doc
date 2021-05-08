9649c88cbdab507f1590acd30f05645ed59d4c4d
Aifedayo <akeemifedayolag@gmail.com>
2021-05-08 12:00:00 +0100

    polls/views.py form handling

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -27,4 +27,9 @@ def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You did not select a choice"
+        })
     return HttpResponse("You are voting on question %s." % question_id)
