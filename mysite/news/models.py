e5ed15b762cc4f77714cec87640da0e691ff1b84
Aifedayo <akeemifedayolag@gmail.com>
2021-05-11 12:00:00 +0100

    django models

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -51,3 +51,4 @@ class Album(models.Model):
 class Runner(models.Model):
     MedalType = models.TextChoices('MedalType', 'GOLD SILVER BRONZE')
     name = models.CharField(max_length=60)
+    medal = models.CharField(blank=True, choices=MedalType.choices, max_length=10)
