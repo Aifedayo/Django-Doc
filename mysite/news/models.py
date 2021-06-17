e305605332633d76c1a681cb2da3d13d9d8028e3
Aifedayo <akeemifedayolag@gmail.com>
2021-06-17 12:00:00 +0100

    news app

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -11,3 +11,4 @@ class Article(models.Model):
     pub_date = models.DateField()
     headline = models.CharField(max_length=200)
     content = models.TextField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
