7c265748d2d9044004ea865a555dc10f57f26e00
Aifedayo <akeemifedayolag@gmail.com>
2021-06-21 12:00:00 +0100

    news app

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('articles/<int:year>/', views.year_archive),
     path('articles/<int:year>/<int:month>/', views.month_archive),
+    path('articles/<int:year>/<int:month>/<int:pk>/', views.article_detail),
 ]
