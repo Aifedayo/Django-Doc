3ad2cb58cbfa0572ec590735d9f18fa8f2de6211
Aifedayo <akeemifedayolag@gmail.com>
2021-06-21 12:00:00 +0100

    news app

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('articles/<int:year>/', views.year_archive)
+    path('articles/<int:year>/', views.year_archive),
+    path('articles/<int:year>/<int:month>/', views.month_archive)
 ]
