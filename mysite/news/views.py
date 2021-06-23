fcbbc36ade8e3d46ec64ad490416d2c17f6ff0dd
Aifedayo <akeemifedayolag@gmail.com>
2021-06-23 12:00:00 +0100

    news app

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -5,3 +5,4 @@ from .models import Article
 def year_archive(request, year):
     a_list = Article.objects.filter(pub_year=year)
     context = {'year': year, 'article_list': a_list}
+    return render(request, 'news/year_archive.html', context)
