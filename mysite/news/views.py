367df2395e4cbb644ffa1c8261845b3f9a3b9ed3
Aifedayo <akeemifedayolag@gmail.com>
2021-06-25 12:00:00 +0100

    news app

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from .models import Article
 
 def year_archive(request, year):
-    a_list = Article.objects.filter(pub_year=year)
-    context = {'year': year, 'article_list': a_list}
+    article_list = Article.objects.filter(pub_year=year)
+    context = {'year': year, 'article_list': article_list}
     return render(request, 'news/year_archive.html', context)
