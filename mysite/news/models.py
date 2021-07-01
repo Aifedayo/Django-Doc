48279637d9803d5ed644fc3b4dfa61732a2a7f9e
Aifedayo <akeemifedayolag@gmail.com>
2021-07-01 12:00:00 +0100

    django models

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -28,3 +28,4 @@ class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     instrument = models.CharField(max_length=50)
+    alias = models.CharField(max_length=50)
