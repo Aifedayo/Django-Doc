7c9e77d5f0677f09b3bdf61121e6574e1e724d8e
Aifedayo <akeemifedayolag@gmail.com>
2021-07-02 12:00:00 +0100

    django models

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -20,6 +20,10 @@ class Article(models.Model):
 Practicing Django Models
 """
 class Person(models.Model):
+    SHIRT_SIZES = (
+        
+    )
+
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 #End of Person Model
