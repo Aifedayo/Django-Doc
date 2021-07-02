722c70ede712770a73e526e879290b4b1d104553
Aifedayo <akeemifedayolag@gmail.com>
2021-07-02 12:00:00 +0100

    django models

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -35,3 +35,4 @@ class Album(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
+    num_stars = models.IntegerField()
