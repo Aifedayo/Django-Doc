f235694dee12551367f4ccaa6e64582e64014189
Aifedayo <akeemifedayolag@gmail.com>
2021-08-26 12:00:00 +0100

    polls/views.py GenericViews

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,7 +5,7 @@ app_name = 'polls'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk/', views.DetailView.as_view(), name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
