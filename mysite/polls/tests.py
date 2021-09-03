bbcc3aa35b5895a8e6ac1ae1a9e318782dbf4439
Aifedayo <akeemifedayolag@gmail.com>
2021-09-03 12:00:00 +0100

    polls/tests.py Automated testing

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -5,4 +5,7 @@ from django.utils import timezone
 
 from .models import Question
 
-
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        
