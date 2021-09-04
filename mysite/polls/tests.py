3d82999ba8676465c1eb3ff95a18edd0ed1281b2
Aifedayo <akeemifedayolag@gmail.com>
2021-09-04 12:00:00 +0100

    polls/tests.py Automated testing

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -9,3 +9,4 @@ class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
