422abf09421c1bbeec141dc31ad11ca3516cfd4a
Aifedayo <akeemifedayolag@gmail.com>
2021-09-05 12:00:00 +0100

    polls/tests.py Automated testing

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,7 +13,7 @@ class Question(models.Model):
     # Adding a custom model
     def was_published_recently(self):
         now = timezone.now()
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
