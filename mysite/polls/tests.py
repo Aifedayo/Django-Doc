1d4968e24c615c5d648f4d0ba503ec7d3259bc86
Aifedayo <akeemifedayolag@gmail.com>
2021-09-07 12:00:00 +0100

    polls/tests.py Automated testing

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -10,3 +10,7 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
