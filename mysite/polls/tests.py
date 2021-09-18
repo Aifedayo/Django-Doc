daf5f1fcfdf7df77b655dd45c50a6442c9690174
Aifedayo <akeemifedayolag@gmail.com>
2021-09-18 12:00:00 +0100

    polls/tests.py Automated testing

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -31,15 +31,15 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
-    def create_question(question_text, days):
-        """
-        Create a question with the given `questio_text` and published the given number
-        of `days` offset to now (negative for questions published in the past, positive
-        for questions that have yet to be published).
-        """
-        time = timezone.now() + datetime.timedelta(days=days)
-        return Question.objects.create(question_text=question_text,
-        pub_date=time)
+def create_question(question_text, days):
+    """
+    Create a question with the given `questio_text` and published the given number
+    of `days` offset to now (negative for questions published in the past, positive
+    for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text,
+    pub_date=time)
 
 
 class QuestionIndexViewTests(TestCase):
@@ -58,3 +58,5 @@ class QuestionIndexViewTests(TestCase):
         Questions with a pub_date in the past are displayed on the
         index page.
         """
+        question = create_question(question_text="Past question.", days=-30)
+        
