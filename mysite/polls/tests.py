1edb2f1dd97c26134a428c2a4c65b1a7923d6791
Aifedayo <akeemifedayolag@gmail.com>
2021-09-20 12:00:00 +0100

    polls/tests.py Automated testing

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -72,6 +72,7 @@ class QuestionIndexViewTests(TestCase):
         """
         question = create_question(question_text="Future questions.", days=30)
         response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, 'No polls are available')
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
         )
