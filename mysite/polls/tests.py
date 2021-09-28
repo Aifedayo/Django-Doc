c09e643c257a2f98868f7a71afd936d462fbcbe1
Aifedayo <akeemifedayolag@gmail.com>
2021-09-28 12:00:00 +0100

    polls/tests.py Automated testing

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -112,4 +112,4 @@ class QuestionDetailViewTests(TestCase):
         """
         future_question = create_question(question_text='Future question.', days=5)
         url = reverse('polls:detail', args=(future_question.id,))
-        
+        response = self.client.get(url)
