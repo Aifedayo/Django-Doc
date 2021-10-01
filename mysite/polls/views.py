946ff723a79add9dbb29940110e06069ae9c2ede
Aifedayo <akeemifedayolag@gmail.com>
2021-10-01 12:00:00 +0100

    polls/static style.css

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -23,7 +23,7 @@ class IndexView(generic.ListView):
         """Return the last five published questions"""
         return Question.objects.filter(
             pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        ).exclude().order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
