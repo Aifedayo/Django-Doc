38b2a4dbc437e05474f6938f0fc43fa54e73735d
Aifedayo <akeemifedayolag@gmail.com>
2021-10-02 12:00:00 +0100

    polls/admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
-
 class QuestionAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'question_text']
 
 
+admin.site.register(Question)
+admin.site.register(Choice)
