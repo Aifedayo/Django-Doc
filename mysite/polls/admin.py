100361a4eb8b331fff8679e86087826d906a4286
Aifedayo <akeemifedayolag@gmail.com>
2021-10-04 12:00:00 +0100

    polls/admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Question information',      {'fields': ['question_text']}),
