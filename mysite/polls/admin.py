eb78cb1633e3609c391418ddb07e126769c3c70f
Aifedayo <akeemifedayolag@gmail.com>
2021-10-05 12:00:00 +0100

    polls/admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -12,6 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information',    {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
