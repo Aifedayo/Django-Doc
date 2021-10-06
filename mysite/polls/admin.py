0de9ece2e794580ec291a57cedff2ca8f29428b4
Aifedayo <akeemifedayolag@gmail.com>
2021-10-06 12:00:00 +0100

    polls/admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -13,6 +13,7 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
